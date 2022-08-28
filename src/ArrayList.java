fa15663e6c10ce87768f18169587fe77422ffb0a
TXFK <2023474410@qq.com>
2022-08-28 14:54:37 +0800

    init 2

---
+++ src/ArrayList.java
@@ -0,0 +1,17 @@
+package src;
+
+import java.util.Arrays;
+
+public class ArrayList<T> {
+    T[] values;
+    int size = 0;
+
+    public ArrayList(T[] items){
+        values = Arrays.copyOf(items,items.length);
+        size = items.length;
+    }
+
+    public void add(T item){
+        // todo: finish it.
+    }
+}
---
+++ src/BinaryTree.java
@@ -0,0 +1,40 @@
+package src;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class BinaryTree<T> {
+    public T value;
+    public BinaryTree<T> left;
+    public BinaryTree<T> right;
+
+    public BinaryTree() {
+    }
+
+    public BinaryTree(T value){
+        this.value = value;
+    }
+
+    public BinaryTree(T value, BinaryTree<T> left){
+        this.value = value;
+        this.left = left;
+    }
+    public BinaryTree(T value, BinaryTree<T> left, BinaryTree<T> right){
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+
+    /*——————S————————T————————A————————T————————I———————C————————method————————*/
+    public static Object[] levelOrderTraversal(BinaryTree<?> binaryTree){
+
+        Stack<Object> result = new Stack<>();
+        LinkedList<BinaryTree<?>> binaryTrees = new LinkedList<>();
+        binaryTrees.offer(binaryTree);
+        while(!binaryTrees.isEmpty()){
+            BinaryTree<?> operatingNode = binaryTrees.poll();
+            result.push(operatingNode)
+        }
+    }
+
+}
